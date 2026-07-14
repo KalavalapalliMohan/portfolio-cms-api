8a8e2b1fb1fbadab581751142782cbd39a8de7b1
Kalavalapalli Mohan <kalavalapallimohan9@gmail.com>
2026-07-14 16:05:35 +0530

    Implement Sanctum authentication and login API

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                "success" => false,
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "success" => true,
+            "message" => 'Login successful',
+            'access_token' => $token,
+            'user' => $user,
+            // 'token_type' => 'Bearer',
+        ],200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Api\SettingController;
 use App\Http\Controllers\Api\SkillController;
 use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\SocialLinkController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/settings', [SettingController::class, 'index']);
 Route::get('/skills', [SkillController::class, 'index']);
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/social-links', [SocialLinkController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
